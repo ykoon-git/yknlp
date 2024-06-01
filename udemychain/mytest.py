4250d881d1a1aafb41c1f52775989c28f2b592b5
ykoon <ykoon@me.com>
2024-06-01 18:00:14 +0900

    clear

---
+++ udemychain/mytest.py
@@ -0,0 +1,10 @@
+import os
+from langchain import PromptTemplate
+from langchain.chat_models import OpenAI
+
+if __name__ == '__main__':
+  print("hello langchain~!!!")
+  print(os.environ['OPENAI_API_KEY'])
+  
+
+
